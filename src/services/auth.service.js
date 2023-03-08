ed32e82888cf45edda66c00121bc148279796b21
Mohammad Nabi <mnabimd@gmail.com>
2023-03-08 16:21:02 +0430

    auth service & user service fully migarated

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -54,27 +54,6 @@ const refreshAuth = async (refreshToken) => {
   }
 };
 
-// /**
-//  * Reset password
-//  * @param {string} resetPasswordToken
-//  * @param {string} newPassword
-//  * @returns {Promise}
-//  */
-// const resetPassword = async (resetPasswordToken, newPassword) => {
-//   try {
-//     const resetPasswordTokenDoc = await tokenService.verifyToken(resetPasswordToken, tokenTypes.ACCESS_TOKEN);
-//     const user = await userService.getUserById(resetPasswordTokenDoc.user);
-//     if (!user) {
-//       throw new Error();
-//     }
-//     await userService.updateUserById(user.id, { password: newPassword });
-//     // await Token.deleteMany({ user: user.id, type: tokenTypes.RESET_PASSWORD });
-//   } catch (error) {
-//     console.log(error);
-//     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password reset failed');
-//   }
-// };
-
 module.exports = {
   loginUserWithEmailAndPassword,
   logout,
